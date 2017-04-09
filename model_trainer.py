666964fbc61c40fbfdb6989a1f79522b2ea662ed
Vasundhara Gupta <vasundhara.gupta@mail.mcgill.ca>
2017-04-09 05:11:03 -0400

    Second commit

--- model_trainer.py
+++ model_trainer.py
@@ -9,7 +9,6 @@ import copy
 import os
 import time
 
-# import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.autograd import Variable
@@ -19,22 +18,50 @@ import torchvision
 from torchvision import datasets, models, transforms
 
 ROOT_TRAINING_DIR = 'segregated_train'
+ROOT_EVAL_DIR = 'segregated_eval'
 
-def dataset_from_dir(root_dir):
-    data_transform = transforms.Compose([
+ROOT_TEST_DIR = 'X_Test'
+
+DEFAULT_NUM_EPOCHS = 5
+
+
+def training_data_transform():
+    '''Data augmentation and normalization'''
+    return transforms.Compose([
         transforms.RandomSizedCrop(224),  # needs to be 224 pixels at minimum,
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),  # analogous to numpy ndarray
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    return datasets.ImageFolder(root_dir, data_transform)
+
+def eval_data_transform():
+    '''Just normalization with no augmentation'''
+    return transforms.Compose([
+        transforms.Scale(256),
+        transforms.CenterCrop(224),  # needs to be 224 pixels at minimum
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+
+def training_dataset_from_dir(root_dir):
+    return datasets.ImageFolder(root_dir, training_data_transform())
+
+
+def eval_dataset_from_dir(root_dir):
+    return datasets.ImageFolder(root_dir, eval_data_transform())
 
 
 def loader_from_dataset(dataset):
-    return torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
+    return torch.utils.data.DataLoader(dataset,
+                                       batch_size=4,  # 4
+                                       shuffle=True,  # True
+                                       num_workers=4)  # 4
 
-def train_model(model, training_dataset_loader, criterion, optim_scheduler, num_epochs=25):
+
+def train_model(model, phase_to_dataset_loader, phase_to_dataset_size, criterion, optim_scheduler,
+                num_epochs=DEFAULT_NUM_EPOCHS):
     since = time.time()
 
     best_model = model
@@ -44,41 +71,54 @@ def train_model(model, training_dataset_loader, criterion, optim_scheduler, num_
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'eval']:
+            if phase == 'train':
+                optimizer = optim_scheduler(model, epoch)
 
-        optimizer = optim_scheduler(model, epoch)
+            running_loss = 0.0
+            running_corrects = 0
 
-        running_loss = 0.0
-        running_corrects = 0
+            dataset = phase_to_dataset_loader[phase].dataset
 
-        # Iterate over data.
-        for data in training_dataset_loader:
-            print('iterating')
-            # get the inputs
-            inputs, labels = data
-            inputs, labels = Variable(inputs), Variable(labels)
+            # Iterate over data.
+            for i, data in enumerate(phase_to_dataset_loader[phase]):
+                # get the inputs and wrapin variable
+                inputs, labels = data
+                
+                inputs, labels = Variable(inputs), Variable(labels)
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-            # forward
-            outputs = model(inputs)
-            _, preds = torch.max(outputs.data, 1)
-            loss = criterion(outputs, labels)
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
 
-            loss.backward()
-            optimizer.step()
+                loss = criterion(outputs, labels)
 
-            # statistics
-            running_loss += loss.data[0]
-            running_corrects += torch.sum(preds == labels.data)
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
 
-        epoch_loss = running_loss / dset_sizes[phase]
-        epoch_acc = running_corrects / dset_sizes[phase]
+                # statistics
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(preds == labels.data)
 
-        print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-            phase, epoch_loss, epoch_acc))
+            dataset_size = phase_to_dataset_size[phase]
+            epoch_loss = running_loss / dataset_size
+            epoch_acc = running_corrects / dataset_size
 
-        print()
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'eval' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model = copy.deepcopy(model)
+
+        print('****')
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
@@ -86,25 +126,55 @@ def train_model(model, training_dataset_loader, criterion, optim_scheduler, num_
     print('Best val Acc: {:4f}'.format(best_acc))
     return best_model
 
+
 def optim_scheduler_ft(model, epoch, init_lr=0.001, lr_decay_epoch=7):
-    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
+    '''Learning rate scheduler'''
+    lr = init_lr * (0.1 ** (epoch // lr_decay_epoch))
 
     if epoch % lr_decay_epoch == 0:
-        print('LR is set to {}'.format(lr))
+        print('Learning rate set to {}'.format(lr))
 
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
     return optimizer
 
-if __name__ == '__main__':
-    dataset = dataset_from_dir(ROOT_TRAINING_DIR)
-    dataset_loader = loader_from_dataset(dataset)
+def pretrained_resnet_model():
+    # 18-layer model
+    model = models.resnet18(pretrained=True)  # pretrained on imagenet
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 2)
+    return model
 
-    inputs, classes = next(iter(dataset_loader))
+if __name__ == '__main__':
+    phase_to_dataset = {
+        'train': training_dataset_from_dir(ROOT_TRAINING_DIR),
+        'eval': eval_dataset_from_dir(ROOT_EVAL_DIR)
+    }
+
+    phase_to_dataset_size = {x: len(phase_to_dataset[x]) for x in phase_to_dataset
+    }
+
+    phase_to_dataset_loader = {
+        x: loader_from_dataset(phase_to_dataset[x]) for x in phase_to_dataset
+    }
 
     model = models.resnet18(pretrained=True)  # pretrained on imagenet
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
-    criterion = nn.CrossEntropyLoss()
 
-    model = train_model(model, dataset_loader, criterion, optim_scheduler_ft, num_epochs=5)
+    model = train_model(
+        model=model,
+        phase_to_dataset_loader=phase_to_dataset_loader,
+        phase_to_dataset_size=phase_to_dataset_size,
+        criterion=nn.CrossEntropyLoss(),
+        optim_scheduler=optim_scheduler_ft,
+        num_epochs=DEFAULT_NUM_EPOCHS)
 
+    # test_dataset = eval_dataset_from_dir(ROOT_TEST_DIR)
+    # test_dataset_loader = loader_from_dataset(test_dataset)
+
+    '''
+    for i, data in enumerate(phase_to_dataset_loader['eval']):
+        print(data)
+        break
+    print(phase_to_dataset_loader['eval'].dataset.imgs)
+    '''
--- training_segregator.py
+++ training_segregator.py
@@ -1,12 +1,20 @@
 '''
+Team Valuca:
 Vasundhara Gupta
 Raluca Niti
 
-This is a script to segregate the files into the following directory structure:
+This is a script to segregate the provided labeled images into the following directory structure based on their images.
 
 /train
     /cats
     /dogs
+/eval
+    /cats
+    /dogs
+
+From the original 6k labelled images, we allocate roughly 75% to a training set and 25% to a labelled eval set (separate from the final, initially unlabelled, test set).
+
+We decided on this split after reading http://stackoverflow.com/questions/13610074/is-there-a-rule-of-thumb-for-how-to-divide-a-dataset-into-training-and-validatio
 '''
 
 import os
@@ -16,21 +24,64 @@ LABELS_CSV = 'Y_Train.csv'
 ORIGINAL_COMBINED_TRAIN_DIR = 'X_Train'
 
 NEW_ROOT_TRAIN_DIR = 'segregated_train'
+NEW_ROOT_EVAL_DIR = 'segregated_eval'
 
-CATS_DIR = os.path.join(NEW_ROOT_TRAIN_DIR, 'cats')
-DOGS_DIR = os.path.join(NEW_ROOT_TRAIN_DIR, 'dogs')
+CATS_DIR = 'cats'
+DOGS_DIR = 'dogs'
+
+
+def ensure_dest_directories_exist():
+    if not os.path.exists(NEW_ROOT_TRAIN_DIR):
+        os.makedirs(NEW_ROOT_TRAIN_DIR)
+    if not os.path.exists(NEW_ROOT_EVAL_DIR):
+        os.makedirs(NEW_ROOT_EVAL_DIR)
+
+    train_cat_dir = os.path.join(NEW_ROOT_TRAIN_DIR, CATS_DIR)
+    if not os.path.exists(train_cat_dir):
+        os.makedirs(train_cat_dir)
+
+    train_dog_dir = os.path.join(NEW_ROOT_TRAIN_DIR, DOGS_DIR)
+    if not os.path.exists(train_dog_dir):
+        os.makedirs(train_dog_dir)
+
+    eval_cat_dir = os.path.join(NEW_ROOT_EVAL_DIR, CATS_DIR)
+    if not os.path.exists(eval_cat_dir):
+        os.makedirs(eval_cat_dir)
+
+    eval_dog_dir = os.path.join(NEW_ROOT_EVAL_DIR, DOGS_DIR)
+    if not os.path.exists(eval_dog_dir):
+        os.makedirs(eval_dog_dir)
+
 
 if __name__ == '__main__':
+    ensure_dest_directories_exist()
+
     f = open(LABELS_CSV, 'r')
     f.readline()  # skip first line
 
+    num_cats_so_far = 0
+    num_dogs_so_far = 0
+
     for line in f:
         image_filename, label = tuple(line.split(','))
+        orig_absolute_path = os.path.join(ORIGINAL_COMBINED_TRAIN_DIR,
+                                          image_filename)
 
-        orig_absolute_path = os.path.join(ORIGINAL_COMBINED_TRAIN_DIR, image_filename)
-        dest_absolute_path = os.path.join(CATS_DIR if int(label) == 0 else DOGS_DIR, image_filename)
-        
+        dest_absolute_path = ''
+        if int(label) == 0:  # cat
+            dest_absolute_path = os.path.join(NEW_ROOT_EVAL_DIR
+                                              if num_cats_so_far % 4 == 0 else
+                                              NEW_ROOT_TRAIN_DIR, CATS_DIR,
+                                              image_filename)
+            num_cats_so_far += 1
+        else:  # dog
+            dest_absolute_path = os.path.join(NEW_ROOT_EVAL_DIR
+                                              if num_dogs_so_far % 4 == 0 else
+                                              NEW_ROOT_TRAIN_DIR, DOGS_DIR,
+                                              image_filename)
+            num_dogs_so_far += 1
+
         try:
             shutil.copyfile(orig_absolute_path, dest_absolute_path)
         except IOError as e:
-            print(e)
+            print(e)  # shouldn't happen
